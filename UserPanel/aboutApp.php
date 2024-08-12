73c388af2a8fe11d9978748479bccd26ca9ecbd4
Mahdi Mosazadeh <MahdiMosazadeh1997@gmail.com>
2024-08-12 23:31:27 +0430

    just write AboutApp page info and texts.

--- UserPanel/aboutApp.php
+++ UserPanel/aboutApp.php
@@ -1,1 +1,39 @@
-about app
+<!DOCTYPE html>
+<html lang="fa">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="image/png" href="../Assets/Images/finger.png" />
+    <title>اشخاص</title>
+
+    <link rel="stylesheet" href="../Assets/Css/fonts.css">
+    <link rel="stylesheet" href="../Assets/Css/bootstrap.rtl.min.css">
+    <link rel="stylesheet" href="../Assets/Css/Style.css">
+    <link rel="stylesheet" href="../Assets/Css/all.css">
+</head>
+<body dir="rtl">
+    <main class="container updateProduct">
+        <section class="row phoneNumber">
+
+            <div class="col">
+            
+                    <h3 class="blue-color">
+                        شرکت طرح و توسعه داده فراز
+                    </h3>
+                    <p style="text-align: justify;">
+                        داده فراز یک شرکت نرم افزاری می باشد که فعالیت اصلی آن طراحی و توسعه انواع وب سایت های تخصصی می باشد ، همچنین نرم افزار انبارداری و حسابداری داده فراز با قیمت بسیار مقرون به صرف گزینه ای عالی برای کسب و کار های خرده و کوچک می باشد تا بتوانند با هزینه ای کم امکانات نرم افزارهای انبارداری و حسابداری را برای مدیریت کالا ها و دخل و خرج خود را داشته باشند.
+                    </p>
+                    <p style="text-align: justify;">
+                        راه های ارتباطی با داده فراز : 09146528873
+                    </p>
+                    <p style="text-align: justify;">
+                        تلگرام : @persvens
+                    </p>
+            </div>
+
+        </section>
+    </main>
+<script src="../Assets/Js/bootstrap.bundle.min.js"></script>
+<script src="../Assets/Js/bootstrap.min.js"></script>
+</body>
+</html>
