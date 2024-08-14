69ed21ab2b9665f7306241e0fabc7e4d4a6e3c1a
Mahdi Mosazadeh <MahdiMosazadeh1997@gmail.com>
2024-08-14 23:30:40 +0430

    finish the login system and create users table in mysql.

--- index.php
+++ index.php
@@ -1,3 +1,32 @@
+<?php
+session_start();
+require_once './Scripts/dbConnect.php';
+require_once './Scripts/functions.php';
+
+if (isset($_POST['loginBtn']))
+{
+    //filter user and pass with function cleanUpInputs.
+    $userName = cleanUpInputs($_POST['userName']);
+    $passWord = cleanUpInputs($_POST['passWord']);
+    
+    $sql = $conn->prepare("SELECT * FROM users WHERE username= :user and password= :pass");
+    $sql -> bindParam(':user',$userName);
+    $sql -> bindParam(':pass',$passWord);
+    $sql->execute();
+    $conn = null;
+    
+    if ($sql -> rowCount() > 0)
+    {
+        $_SESSION['logged_in']= $username;
+        redirect('./login/login.php');
+    }
+    else
+    {
+        $incorrect = true;
+    }
+}
+
+?>
 <!DOCTYPE html>
 <html lang="fa">
 <head>
@@ -16,10 +45,11 @@
         <section class="row login-system">
             <div class="col">
                 <img src="./Assets/Images/logo.png" alt="لولوی داده فراز">
-                <form action="./login/login.php" method="post" class="form-control">
-                    <input class="form-control" type="text"  required="required" placeholder="نام کاربری" oninvalid="this.setCustomValidity('لطفا نام کاربری یا یوزرنیم خود را وارد کنید')" oninput="setCustomValidity('')"></input>
-                    <input class="form-control" type="password"  required="required" placeholder="رمز عبور" oninvalid="this.setCustomValidity('لطفا نام گذرواژه یا پسورد خود را وارد کنید')" oninput="setCustomValidity('')"></input>
-                    <button type="submit" class="btn btn-primary form-control" name="select-system">ورود</button>
+                <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post" class="form-control">
+                    <?php if(isset($incorrect)){echo "<script>alert('نام کاربری یا رمزعبور اشتباه است');</script>";} ?>
+                    <input id="userName" name="userName" class="form-control" type="text"  required="required" placeholder="نام کاربری" oninvalid="this.setCustomValidity('لطفا نام کاربری یا یوزرنیم خود را وارد کنید')" oninput="setCustomValidity('')"></input>
+                    <input id="passWord" name="passWord" class="form-control" type="password"  required="required" placeholder="رمز عبور" oninvalid="this.setCustomValidity('لطفا نام گذرواژه یا پسورد خود را وارد کنید')" oninput="setCustomValidity('')"></input>
+                    <button id="loginBtn" name="loginBtn" type="submit" class="btn btn-primary form-control" name="select-system">ورود</button>
                 </form>
             </div>
         </section>
