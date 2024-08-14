2dd5fa55b65de89a726c999839508850cf45490a
Mahdi Mosazadeh <MahdiMosazadeh1997@gmail.com>
2024-08-14 20:46:43 +0430

    switch this two files name's and from now first i check username and password in index.php and next by login.php check system Type

--- Login/login.php
+++ Login/login.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="icon" type="image/png" href="../Assets/Images/finger.png" />
-    <title>صفحه ورود</title>
+    <title>حسابداری | انبارداری داده فراز</title>
 
     <link rel="stylesheet" href="../Assets/Css/fonts.css">
     <link rel="stylesheet" href="../Assets/Css/bootstrap.rtl.min.css">
@@ -13,13 +13,16 @@
 </head>
 <body dir="rtl">
     <main class="container">
-        <section class="row login-system">
+        <section class="row select-system">
             <div class="col">
                 <img src="../Assets/Images/logo.png" alt="لولوی داده فراز">
-                <form action="./check.php" method="post" class="form-control">
-                    <input class="form-control" type="text"  required="required" placeholder="نام کاربری" oninvalid="this.setCustomValidity('لطفا نام کاربری یا یوزرنیم خود را وارد کنید')" oninput="setCustomValidity('')"></input>
-                    <input class="form-control" type="password"  required="required" placeholder="رمز عبور" oninvalid="this.setCustomValidity('لطفا نام گذرواژه یا پسورد خود را وارد کنید')" oninput="setCustomValidity('')"></input>
-                    <button type="submit" class="btn btn-primary form-control" name="select-system">ورود</button>
+                <form action="check.php" method="post" class="form-control">
+                    <label for="select" class="lable-control">سیستم مورد نظر را انتخاب کنید</label>
+                    <select id="select" class="form-select">
+                        <option value="1" disabled>حسابداری</option>
+                        <option value="2">انبارداری</option>
+                    </select>
+                    <button type="submit" class="btn btn-primary form-control" name="select-system">انتخاب سیستم</button>
                 </form>
             </div>
         </section>
