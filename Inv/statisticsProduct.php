0f5ea400784fcc6ba100e58b790e8a21c5a89fde
Mahdi Mosazadeh <MahdiMosazadeh1997@gmail.com>
2024-08-17 23:15:25 +0430

    Create Statistics Page With 3 type Info :(Nothing,Less,All) Products List.

--- Inv/statisticsProduct.php
+++ Inv/statisticsProduct.php
@@ -30,59 +30,47 @@
         <section class="row">
             <div class="col">
                 <form action="" method="post" class="form-control">
-                <h3 class="red-color">
+                    <h3 class="red-color">
                         ناموجود 
                     </h3>
+                    <?php
+                        $searchSqlNothing = $conn -> query('SELECT * FROM products WHERE p_qty=0');
+                        $resultNothing = $searchSqlNothing -> fetchAll(PDO::FETCH_ASSOC);
+                        $num = 1;
+                    ?>
                     <table class="table">
-                    <thead class="thead-dark">
-                        <tr>
-                            <th scope="col">ردیف</th>
-                            <th scope="col">کدینگ</th>
-                            <th scope="col">نام</th>
-                            <th scope="col">استقرار</th>
-                            <th scope="col">واحد</th>
-                            <th scope="col">تعداد</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                    <tr>
-                            <th scope="row">1</th>
-                            <td>14673</td>
-                            <td>آچار فرانسه کوچک</td>
-                            <td>انبار ابزار | قفسه 4</td>
-                            <td>عدد</td>
-                            <td>4</td>
-                        </tr>
-                        <tr>
-                            <th scope="row">2</th>
-                            <td>14674</td>
-                            <td>آچار فرانسه بزرگ</td>
-                            <td>انبار ابزار | قفسه 5</td>
-                            <td>عدد</td>
-                            <td>3</td>
-                        </tr>
-                        <tr>
-                            <th scope="row">3</th>
-                            <td>14673</td>
-                            <td>آچار فرانسه کوچک</td>
-                            <td>انبار ابزار | قفسه 4</td>
-                            <td>عدد</td>
-                            <td>4</td>
-                        </tr>
-                        <tr>
-                            <th scope="row">4</th>
-                            <td>14674</td>
-                            <td>آچار فرانسه بزرگ</td>
-                            <td>انبار ابزار | قفسه 5</td>
-                            <td>عدد</td>
-                            <td>3</td>
-                        </tr>
-                    </tbody>
-                </table>
+                        <thead class="thead-dark">
+                            <tr>
+                                <th scope="col">ردیف</th>
+                                <th scope="col">کدینگ</th>
+                                <th scope="col">نام</th>
+                                <th scope="col">استقرار</th>
+                                <th scope="col">واحد</th>
+                                <th scope="col">تعداد</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php foreach ($resultNothing as $row): ?>
+                                <tr>
+                                    <th scope="row"><?php echo $num++ ?></th>
+                                    <td><?php if(htmlspecialchars($row['p_codeing'])==0) {echo "ندارد";} else {echo htmlspecialchars($row['p_codeing']);}?></td>
+                                    <td><?php echo htmlspecialchars($row['p_name']) ?></td>
+                                    <td><?php echo htmlspecialchars($row['p_place']) ?></td>
+                                    <td><?php echo htmlspecialchars($row['p_unit']) ?></td>
+                                    <td><?php echo htmlspecialchars($row['p_qty']) ?></td>
+                                </tr>
+                            <?php endforeach; ?>
+                        </tbody>
+                    </table>
 
-                <h3 class="orange-color">
+                    <h3 class="orange-color">
                         در آستانه اتمام
                     </h3>
+                    <?php
+                        $searchSqlLess = $conn -> query('SELECT * FROM products WHERE p_qty <= 5 AND p_qty > 0');
+                        $resultLess = $searchSqlLess -> fetchAll(PDO::FETCH_ASSOC);
+                        $num = 1;
+                    ?>
                     <table class="table">
                     <thead class="thead-dark">
                         <tr>
@@ -95,44 +83,30 @@
                         </tr>
                     </thead>
                     <tbody>
-                    <tr>
-                            <th scope="row">1</th>
-                            <td>14673</td>
-                            <td>آچار فرانسه کوچک</td>
-                            <td>انبار ابزار | قفسه 4</td>
-                            <td>عدد</td>
-                            <td>4</td>
-                        </tr>
-                        <tr>
-                            <th scope="row">2</th>
-                            <td>14674</td>
-                            <td>آچار فرانسه بزرگ</td>
-                            <td>انبار ابزار | قفسه 5</td>
-                            <td>عدد</td>
-                            <td>3</td>
-                        </tr>
-                        <tr>
-                            <th scope="row">3</th>
-                            <td>14673</td>
-                            <td>آچار فرانسه کوچک</td>
-                            <td>انبار ابزار | قفسه 4</td>
-                            <td>عدد</td>
-                            <td>4</td>
-                        </tr>
-                        <tr>
-                            <th scope="row">4</th>
-                            <td>14674</td>
-                            <td>آچار فرانسه بزرگ</td>
-                            <td>انبار ابزار | قفسه 5</td>
-                            <td>عدد</td>
-                            <td>3</td>
-                        </tr>
+                            <?php foreach ($resultLess as $row): ?>
+                                <tr>
+                                    <th scope="row"><?php echo $num++ ?></th>
+                                    <td><?php if(htmlspecialchars($row['p_codeing'])==0) {echo "ندارد";} else {echo htmlspecialchars($row['p_codeing']);}?></td>
+                                    <td><?php echo htmlspecialchars($row['p_name']) ?></td>
+                                    <td><?php echo htmlspecialchars($row['p_place']) ?></td>
+                                    <td><?php echo htmlspecialchars($row['p_unit']) ?></td>
+                                    <td><?php echo htmlspecialchars($row['p_qty']) ?></td>
+                                </tr>
+                            <?php endforeach; ?>
                     </tbody>
                 </table>
 
-                <h3 class="blue-color">
+                    <h3 class="blue-color">
                         لیست کامل کالا ها
                     </h3>
+                    <p>
+                        به جز ناموجود ها و در آستانه اتمام ها
+                    </p>
+                    <?php
+                        $searchSqlAll = $conn -> query('SELECT * FROM products WHERE p_qty > 5');
+                        $resultAll = $searchSqlAll -> fetchAll(PDO::FETCH_ASSOC);
+                        $num = 1;
+                    ?>
                     <table class="table">
                     <thead class="thead-dark">
                         <tr>
@@ -145,38 +119,16 @@
                         </tr>
                     </thead>
                     <tbody>
-                    <tr>
-                            <th scope="row">1</th>
-                            <td>14673</td>
-                            <td>آچار فرانسه کوچک</td>
-                            <td>انبار ابزار | قفسه 4</td>
-                            <td>عدد</td>
-                            <td>4</td>
-                        </tr>
-                        <tr>
-                            <th scope="row">2</th>
-                            <td>14674</td>
-                            <td>آچار فرانسه بزرگ</td>
-                            <td>انبار ابزار | قفسه 5</td>
-                            <td>عدد</td>
-                            <td>3</td>
-                        </tr>
-                        <tr>
-                            <th scope="row">3</th>
-                            <td>14673</td>
-                            <td>آچار فرانسه کوچک</td>
-                            <td>انبار ابزار | قفسه 4</td>
-                            <td>عدد</td>
-                            <td>4</td>
-                        </tr>
-                        <tr>
-                            <th scope="row">4</th>
-                            <td>14674</td>
-                            <td>آچار فرانسه بزرگ</td>
-                            <td>انبار ابزار | قفسه 5</td>
-                            <td>عدد</td>
-                            <td>3</td>
-                        </tr>
+                            <?php foreach ($resultAll as $row): ?>
+                                <tr>
+                                    <th scope="row"><?php echo $num++ ?></th>
+                                    <td><?php if(htmlspecialchars($row['p_codeing'])==0) {echo "ندارد";} else {echo htmlspecialchars($row['p_codeing']);}?></td>
+                                    <td><?php echo htmlspecialchars($row['p_name']) ?></td>
+                                    <td><?php echo htmlspecialchars($row['p_place']) ?></td>
+                                    <td><?php echo htmlspecialchars($row['p_unit']) ?></td>
+                                    <td><?php echo htmlspecialchars($row['p_qty']) ?></td>
+                                </tr>
+                            <?php endforeach; ?>
                     </tbody>
                 </table>
 
