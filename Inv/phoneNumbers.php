025a5cf3fe5b3cba95f5cff48c5e118f72d86d5a
Mahdi Mosazadeh <MahdiMosazadeh1997@gmail.com>
2024-08-19 21:17:23 +0430

    Inv/phoneNumbers.php

--- Inv/phoneNumbers.php
+++ Inv/phoneNumbers.php
@@ -11,6 +11,33 @@
     }
     else
     {
+        //check Btn Are Clicked Or Not 
+        if(isset($_POST['persBtn']))
+        {
+            //recive form Paramets
+            $flName = cleanUpInputs($_POST['flName']);
+            $persType = cleanUpInputs($_POST['persType']);
+            $persAddress = cleanUpInputs($_POST['persAddress']);
+            $persNumber = cleanUpInputs($_POST['persNumber']);
+            
+            //Insert SQL
+            try
+            {
+                $sqlInsertPers = $conn -> prepare("INSERT INTO `persons` (`id`, `flName`, `persType`, `persAddress`, `persNumber`) VALUES (NULL, ?, ?, ?, ?);");
+                $sqlInsertPers -> execute([$flName,$persType,$persAddress,$persNumber]);
+                // if query execute successfully then Alert Success 
+                if($sqlInsertPers)
+                {
+                    $successMessage = 1;
+                }
+            }
+            catch (\PDOException $e)
+            {  
+                    throw new \PDOException($e->getMessage(), (int)$e->getCode());
+                    $error = $e;
+                    $errorMessage = 1;
+            }  
+        }
 ?>
 <!DOCTYPE html>
 <html lang="fa">
@@ -24,21 +51,49 @@
     <link rel="stylesheet" href="../Assets/Css/bootstrap.rtl.min.css">
     <link rel="stylesheet" href="../Assets/Css/Style.css">
     <link rel="stylesheet" href="../Assets/Css/all.css">
+    <link rel="stylesheet" href="../Assets/Css/sweetalert2.min.css"> 
+    <script src="../Assets/Js/sweetalert2.js"></script>
 </head>
 <body dir="rtl">
     <main class="container updateProduct">
         <section class="row phoneNumber">
 
             <div class="col col-12 col-sm-12 col-md-4">
-            <form action="" method="post" class="form-control">
+            <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="post" class="form-control">
                     <h3 class="blue-color">
                         شخص جدید
                     </h3>
-                    <input type="text" name="" id="" class="form-control" style="direction: rtl" placeholder="نام و نام خانوادگی" oninvalid="this.setCustomValidity('لطفا نام و نام خانوادگی شخص را وارد کنید')" oninput="setCustomValidity('')">
-                    <input type="text" name="" id="" class="form-control" style="direction: rtl" placeholder="نوع (مثلا : شخص ، شرکت ، مشتری و ...)" required="required" oninvalid="this.setCustomValidity('نوع شخص یا شرکت را وارد کنید')" oninput="setCustomValidity('')">
-                    <input type="text" name="" id="" class="form-control" style="direction: rtl" placeholder="آدرس" required="required" oninvalid="this.setCustomValidity('آدرس را وارد کنید')" oninput="setCustomValidity('')">
-                    <input type="number" name="" id="" class="form-control" min="0" step="1" style="direction: rtl" placeholder="شماره تماس" required="required" oninvalid="this.setCustomValidity('تلفن را وارد کنید')" oninput="setCustomValidity('')">
-                    <button type="submit"  class="btn btn-primary">ثبت شخص</button>
+                    <?php
+                    //Success Insert Message
+                    if(isset($successMessage))
+                    {
+                     echo '<script type="text/javascript">  
+                            Swal.fire
+                                    ({  
+                                        text: "شخص جدید با موفقیت ثبت شد", 
+                                        icon: "success", 
+                                        confirmButtonText: "تأیید"  
+                                    });
+                      </script>';
+                    }
+                    //Error Insert Message
+                    else if(isset($errorMessage))
+                    {
+                     echo '<script type="text/javascript">  
+                            Swal.fire
+                                    ({    
+                                        text: "'.$e.'",  
+                                        icon: "error", 
+                                        confirmButtonText: "تأیید"  
+                                    });
+                      </script>';
+                    }
+                ?>
+                    <input type="text" name="flName" id="flName" class="form-control" style="direction: rtl" placeholder="نام و نام خانوادگی" required="required" oninvalid="this.setCustomValidity('لطفا نام و نام خانوادگی شخص را وارد کنید')" oninput="setCustomValidity('')">
+                    <input type="text" name="persType" id="persType" class="form-control" style="direction: rtl" placeholder="نوع (مثلا : شخص ، شرکت ، مشتری و ...)" required="required" oninvalid="this.setCustomValidity('نوع شخص یا شرکت را وارد کنید')" oninput="setCustomValidity('')">
+                    <input type="text" name="persAddress" id="persAddress" class="form-control" style="direction: rtl" placeholder="آدرس" required="required" oninvalid="this.setCustomValidity('آدرس را وارد کنید')" oninput="setCustomValidity('')">
+                    <input type="number" name="persNumber" id="persNumber" class="form-control" style="direction: rtl" placeholder="شماره تماس" required="required" oninvalid="this.setCustomValidity('تلفن را وارد کنید')" oninput="setCustomValidity('')">
+                    <button type="submit" name="persBtn" id="persBtn" class="btn btn-primary">ثبت شخص</button>
                 </form>
             </div>
 
@@ -47,6 +102,12 @@
                 <h3 class="blue-color">
                     لیست اشخاص
                 </h3>
+                <?php
+                        $searchSqlNothing = $conn -> query('SELECT * FROM persons');
+                        $resultPers = $searchSqlNothing -> fetchAll(PDO::FETCH_ASSOC);
+                        $conn = null;
+                        $num = 1;
+                    ?>
                 <table class="table">
                     <thead class="thead-dark">
                         <tr>
@@ -58,34 +119,15 @@
                         </tr>
                     </thead>
                     <tbody>
-                        <tr>
-                            <th scope="row">1</th>
-                            <td>مهدی موسی زاده</td>
-                            <td>مشتری</td>
-                            <td>تبریز، میدان آذربایجان ، پایگاه دوم شکاری</td>
-                            <td>09146528873</td>
-                        </tr>
-                        <tr>
-                            <th scope="row">2</th>
-                            <td>داده فراز</td>
-                            <td>شرکت</td>
-                            <td>تبریز،آخرشهناز، چهارراه طالقانی قدیم</td>
-                            <td>09146528873</td>
-                        </tr>
-                        <tr>
-                            <th scope="row">3</th>
-                            <td>مهدی موسی زاده</td>
-                            <td>مشتری</td>
-                            <td>تبریز، میدان آذربایجان ، پایگاه دوم شکاری</td>
-                            <td>09146528873</td>
-                        </tr>
-                        <tr>
-                            <th scope="row">4</th>
-                            <td>مهدی موسی زاده</td>
-                            <td>مشتری</td>
-                            <td>تبریز، میدان آذربایجان ، پایگاه دوم شکاری</td>
-                            <td>09146528873</td>
-                        </tr>
+                            <?php foreach ($resultPers as $row): ?>
+                                <tr>
+                                    <th scope="row"><?php echo $num++ ?></th>
+                                    <td><?php echo htmlspecialchars($row['flName']) ?></td>
+                                    <td><?php echo htmlspecialchars($row['persType']) ?></td>
+                                    <td><?php echo htmlspecialchars($row['persAddress']) ?></td>
+                                    <td><?php echo htmlspecialchars($row['persNumber']) ?></td>
+                                </tr>
+                            <?php endforeach; ?>
                     </tbody>
                 </table>
                 </form>
