f543cf1e1c399531b651918e215197748d576e7a
Mahdi Mosazadeh <MahdiMosazadeh1997@gmail.com>
2024-08-20 23:49:18 +0430

    create product search section in searchproduct.php

--- Inv/searchProduct.php
+++ Inv/searchProduct.php
@@ -11,6 +11,18 @@
     }
     else
     {
+        if(isset($_POST['productSearchBtn']))
+        {
+            $productName = $_POST['productName'];
+            
+            $num = 1;
+
+            $searchProductSql = $conn -> prepare("SELECT * FROM `products` WHERE `p_name` LIKE :search");  
+            $searchProductSql -> execute(['search' => '%' . $productName . '%']);  
+
+            // دریافت نتایج  
+            $searchProductSqlResult = $searchProductSql->fetchAll(PDO::FETCH_ASSOC);
+        }
 ?>
 <!DOCTYPE html>
 <html lang="fa">
@@ -18,25 +30,41 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="icon" type="image/png" href="../Assets/Images/finger.png" />
-    <title>معرفی کالا</title>
+    <title>جستجوی کالا</title>
 
     <link rel="stylesheet" href="../Assets/Css/fonts.css">
     <link rel="stylesheet" href="../Assets/Css/bootstrap.rtl.min.css">
     <link rel="stylesheet" href="../Assets/Css/Style.css">
     <link rel="stylesheet" href="../Assets/Css/all.css">
+    <link rel="stylesheet" href="../Assets/Css/sweetalert2.min.css"> 
+    <script src="../Assets/Js/sweetalert2.js"></script>
 </head>
 <body dir="rtl">
     <main class="container searchProduct">
         <section class="row">
             <div class="col">
-                <form action="" method="post" class="form-control">
+                <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="post" class="form-control">
                     <h3 class="blue-color">
                         جستجوی کالا
                     </h3>
                     <div class="input-group mb-3">
-                        <input type="text" class="form-control" placeholder="قسمتی از نام کالا را وارد کنید" aria-label="Example text with button addon" aria-describedby="button-addon1">
-                        <button class="btn btn-outline-secondary" type="button" id="button-addon1">جستجو</button>
+                        <input type="text" name="productName" class="form-control" required="required" placeholder="قسمتی از نام کالا را وارد کنید" oninvalid="this.setCustomValidity('قسمتی از نام کالا را وارد کنید')" oninput="setCustomValidity('')">
+                        <button class="btn btn-outline-secondary" name="productSearchBtn" type="submit" id="button-addon1">جستجو</button>
                     </div>
+                    <?php
+                     if(isset($searchProductSqlResult)) {
+                        if ($searchProductSql -> rowCount() == 0 )
+                        {
+                            echo '<script type="text/javascript">  
+                            Swal.fire
+                                    ({    
+                                        text: "کالایی با این نام وجود ندارد",  
+                                        icon: "error", 
+                                        confirmButtonText: "تأیید"  
+                                    });
+                      </script>';
+                        }    
+                    ?>
                     <table class="table">
                     <thead class="thead-dark">
                         <tr>
@@ -46,43 +74,24 @@
                             <th scope="col">استقرار</th>
                             <th scope="col">واحد</th>
                             <th scope="col">تعداد</th>
+                            <th scope="col">توضیحات</th>
                         </tr>
                     </thead>
                     <tbody>
-                    <tr>
-                            <th scope="row">1</th>
-                            <td>14673</td>
-                            <td>آچار فرانسه کوچک</td>
-                            <td>انبار ابزار | قفسه 4</td>
-                            <td>عدد</td>
-                            <td>4</td>
-                        </tr>
-                        <tr>
-                            <th scope="row">2</th>
-                            <td>14674</td>
-                            <td>آچار فرانسه بزرگ</td>
-                            <td>انبار ابزار | قفسه 5</td>
-                            <td>عدد</td>
-                            <td>3</td>
-                        </tr>
-                        <tr>
-                            <th scope="row">3</th>
-                            <td>14673</td>
-                            <td>آچار فرانسه کوچک</td>
-                            <td>انبار ابزار | قفسه 4</td>
-                            <td>عدد</td>
-                            <td>4</td>
-                        </tr>
-                        <tr>
-                            <th scope="row">4</th>
-                            <td>14674</td>
-                            <td>آچار فرانسه بزرگ</td>
-                            <td>انبار ابزار | قفسه 5</td>
-                            <td>عدد</td>
-                            <td>3</td>
-                        </tr>
+                            <?php foreach ($searchProductSqlResult as $row): ?>
+                                <tr>
+                                    <th scope="row"><?php echo $num++ ?></th>
+                                    <td><?php if(htmlspecialchars($row['p_codeing'])==0) {echo "ندارد";} else {echo htmlspecialchars($row['p_codeing']);}?></td>
+                                    <td><?php echo htmlspecialchars($row['p_name']) ?></td>
+                                    <td><?php echo htmlspecialchars($row['p_place']) ?></td>
+                                    <td><?php echo htmlspecialchars($row['p_unit']) ?></td>
+                                    <td><?php echo htmlspecialchars($row['p_qty']) ?></td>
+                                    <td><?php echo htmlspecialchars($row['p_description']) ?></td>
+                                </tr>
+                            <?php endforeach; ?>
                     </tbody>
-                </table>
+                    </table>
+                    <?php } ?>
                 </form>
             </div>
         </section>
