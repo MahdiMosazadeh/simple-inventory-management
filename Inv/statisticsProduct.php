a396ea10cd3af3159a78be9021bf5cad38018457
Mahdi Mosazadeh <MahdiMosazadeh1997@gmail.com>
2024-09-09 00:52:35 +0430

    Add An Delete Product Link And User Can Del Product.

--- Inv/statisticsProduct.php
+++ Inv/statisticsProduct.php
@@ -1,146 +1,179 @@
 <?php
-    session_start();
-    require_once '../Scripts/dbConnect.php';
-    require_once '../Scripts/functions.php';
+session_start();
+require_once '../Scripts/dbConnect.php';
+require_once '../Scripts/functions.php';
 
-    //Check the User Session Login , If Session Doesn't Set
-    //Then Redirect To The Login Page And If Login Is Set Show The Page
-    if(!isset($_SESSION['logged_in']))
-    {
-        redirect('../');
+//Check the User Session Login , If Session Doesn't Set
+//Then Redirect To The Login Page And If Login Is Set Show The Page
+if (!isset($_SESSION['logged_in'])) {
+    redirect('../');
+} else {
+    //delete product by id
+    if (isset($_GET['id'])) {
+        $id = cleanUpInputs($_GET['id']);
+
+        $sql = $conn->prepare("DELETE FROM `products` WHERE `products`.`id` = ?");
+        $sql->bindParam(1, $id);
+        $sql->execute();
     }
-    else
-    {
 ?>
-<!DOCTYPE html>
-<html lang="fa">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="icon" type="image/png" href="../Assets/Images/finger.png" />
-    <title>معرفی کالا</title>
+    <!DOCTYPE html>
+    <html lang="fa">
 
-    <link rel="stylesheet" href="../Assets/Css/fonts.css">
-    <link rel="stylesheet" href="../Assets/Css/bootstrap.rtl.min.css">
-    <link rel="stylesheet" href="../Assets/Css/Style.css">
-    <link rel="stylesheet" href="../Assets/Css/all.css">
-</head>
-<body dir="rtl">
-    <main class="container statisticsProduct">
-        <section class="row">
-            <div class="col">
-                <form action="" method="post" class="form-control">
-                    <h3 class="red-color">
-                        ناموجود 
-                    </h3>
-                    <?php
-                        $searchSqlNothing = $conn -> query('SELECT * FROM products WHERE p_qty=0 or p_qty < 0');
-                        $resultNothing = $searchSqlNothing -> fetchAll(PDO::FETCH_ASSOC);
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="icon" type="image/png" href="../Assets/Images/finger.png" />
+        <title>معرفی کالا</title>
+
+        <link rel="stylesheet" href="../Assets/Css/fonts.css">
+        <link rel="stylesheet" href="../Assets/Css/bootstrap.rtl.min.css">
+        <link rel="stylesheet" href="../Assets/Css/Style.css">
+        <link rel="stylesheet" href="../Assets/Css/all.css">
+    </head>
+
+    <body dir="rtl">
+        <main class="container statisticsProduct">
+            <section class="row">
+                <div class="col">
+                    <form action="" method="post" class="form-control">
+                        <h3 class="red-color">
+                            ناموجود
+                        </h3>
+                        <?php
+                        $searchSqlNothing = $conn->query('SELECT * FROM products WHERE p_qty=0 or p_qty < 0');
+                        $resultNothing = $searchSqlNothing->fetchAll(PDO::FETCH_ASSOC);
                         $num = 1;
-                    ?>
-                    <table class="table">
-                        <thead class="thead-dark">
-                            <tr>
-                                <th scope="col">ردیف</th>
-                                <th scope="col">کدینگ</th>
-                                <th scope="col">نام</th>
-                                <th scope="col">استقرار</th>
-                                <th scope="col">واحد</th>
-                                <th scope="col">تعداد</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <?php foreach ($resultNothing as $row): ?>
+                        ?>
+                        <table class="table">
+                            <thead class="thead-dark">
                                 <tr>
-                                    <th scope="row"><?php echo $num++ ?></th>
-                                    <td><?php if(htmlspecialchars($row['p_codeing'])==0) {echo "ندارد";} else {echo htmlspecialchars($row['p_codeing']);}?></td>
-                                    <td><?php echo htmlspecialchars($row['p_name']) ?></td>
-                                    <td><?php echo htmlspecialchars($row['p_place']) ?></td>
-                                    <td><?php echo htmlspecialchars($row['p_unit']) ?></td>
-                                    <td><?php echo htmlspecialchars($row['p_qty']) ?></td>
+                                    <th scope="col">ردیف</th>
+                                    <th scope="col">کدینگ</th>
+                                    <th scope="col">نام</th>
+                                    <th scope="col">استقرار</th>
+                                    <th scope="col">واحد</th>
+                                    <th scope="col">تعداد</th>
+                                    <th scope="col" style="color: red;">حذف</th>
+                                    <th scope="col" style="color: blue;">اصلاح</th>
                                 </tr>
-                            <?php endforeach; ?>
-                        </tbody>
-                    </table>
+                            </thead>
+                            <tbody>
+                                <?php foreach ($resultNothing as $row): ?>
+                                    <tr>
+                                        <th scope="row"><?php echo $num++ ?></th>
+                                        <td><?php if (htmlspecialchars($row['p_codeing']) == 0) {
+                                                echo "ندارد";
+                                            } else {
+                                                echo htmlspecialchars($row['p_codeing']);
+                                            } ?></td>
+                                        <td><?php echo htmlspecialchars($row['p_name']) ?></td>
+                                        <td><?php echo htmlspecialchars($row['p_place']) ?></td>
+                                        <td><?php echo htmlspecialchars($row['p_unit']) ?></td>
+                                        <td><?php echo htmlspecialchars($row['p_qty']) ?></td>
+                                        <td><a style="color: black;" href="?id=<?php echo htmlspecialchars($row['id']) ?>"><i style="margin-right: 5px;" onmouseout="this.style.color='black';" onmouseover="this.style.color='red';" class="fa-thin fa-bin-recycle"></i></a></td>
+                                        <td><a style="color: black;" href="updateProduct.php?id=<?php echo htmlspecialchars($row['id']) ?>"><i style="margin-right: 10px;" onmouseout="this.style.color='black';" onmouseover="this.style.color='blue';" class="fa-thin fa-pen-to-square edit-icon"></i></a></td>
 
-                    <h3 class="orange-color">
-                        در آستانه اتمام
-                    </h3>
-                    <?php
-                        $searchSqlLess = $conn -> query('SELECT * FROM products WHERE p_qty <= 5 AND p_qty > 0');
-                        $resultLess = $searchSqlLess -> fetchAll(PDO::FETCH_ASSOC);
+                                    </tr>
+                                <?php endforeach; ?>
+                            </tbody>
+                        </table>
+
+                        <h3 class="orange-color">
+                            در آستانه اتمام
+                        </h3>
+                        <?php
+                        $searchSqlLess = $conn->query('SELECT * FROM products WHERE p_qty <= 5 AND p_qty > 0');
+                        $resultLess = $searchSqlLess->fetchAll(PDO::FETCH_ASSOC);
                         $num = 1;
-                    ?>
-                    <table class="table">
-                    <thead class="thead-dark">
-                        <tr>
-                            <th scope="col">ردیف</th>
-                            <th scope="col">کدینگ</th>
-                            <th scope="col">نام</th>
-                            <th scope="col">استقرار</th>
-                            <th scope="col">واحد</th>
-                            <th scope="col">تعداد</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                            <?php foreach ($resultLess as $row): ?>
+                        ?>
+                        <table class="table">
+                            <thead class="thead-dark">
                                 <tr>
-                                    <th scope="row"><?php echo $num++ ?></th>
-                                    <td><?php if(htmlspecialchars($row['p_codeing'])==0) {echo "ندارد";} else {echo htmlspecialchars($row['p_codeing']);}?></td>
-                                    <td><?php echo htmlspecialchars($row['p_name']) ?></td>
-                                    <td><?php echo htmlspecialchars($row['p_place']) ?></td>
-                                    <td><?php echo htmlspecialchars($row['p_unit']) ?></td>
-                                    <td><?php echo htmlspecialchars($row['p_qty']) ?></td>
+                                    <th scope="col">ردیف</th>
+                                    <th scope="col">کدینگ</th>
+                                    <th scope="col">نام</th>
+                                    <th scope="col">استقرار</th>
+                                    <th scope="col">واحد</th>
+                                    <th scope="col">تعداد</th>
+                                    <th scope="col" style="color: red;">حذف</th>
+                                    <th scope="col" style="color: blue;">اصلاح</th>
                                 </tr>
-                            <?php endforeach; ?>
-                    </tbody>
-                </table>
+                            </thead>
+                            <tbody>
+                                <?php foreach ($resultLess as $row): ?>
+                                    <tr>
+                                        <th scope="row"><?php echo $num++ ?></th>
+                                        <td><?php if (htmlspecialchars($row['p_codeing']) == 0) {
+                                                echo "ندارد";
+                                            } else {
+                                                echo htmlspecialchars($row['p_codeing']);
+                                            } ?></td>
+                                        <td><?php echo htmlspecialchars($row['p_name']) ?></td>
+                                        <td><?php echo htmlspecialchars($row['p_place']) ?></td>
+                                        <td><?php echo htmlspecialchars($row['p_unit']) ?></td>
+                                        <td><?php echo htmlspecialchars($row['p_qty']) ?></td>
+                                        <td><a style="color: black;" href="?id=<?php echo htmlspecialchars($row['id']) ?>"><i style="margin-right: 5px;" onmouseout="this.style.color='black';" onmouseover="this.style.color='red';" class="fa-thin fa-bin-recycle"></i></a></td>
+                                        <td><a style="color: black;" href="#"><i style="margin-right: 10px;" onmouseout="this.style.color='black';" onmouseover="this.style.color='blue';" class="fa-thin fa-pen-to-square edit-icon"></i></a></td>
+                                    </tr>
+                                <?php endforeach; ?>
+                            </tbody>
+                        </table>
 
-                    <h3 class="blue-color">
-                        لیست کامل کالا ها
-                    </h3>
-                    <p>
-                        به جز ناموجود ها و در آستانه اتمام ها
-                    </p>
-                    <?php
-                        $searchSqlAll = $conn -> query('SELECT * FROM products WHERE p_qty > 5');
-                        $resultAll = $searchSqlAll -> fetchAll(PDO::FETCH_ASSOC);
+                        <h3 class="blue-color">
+                            لیست کامل کالا ها
+                        </h3>
+                        <p>
+                            به جز ناموجود ها و در آستانه اتمام ها
+                        </p>
+                        <?php
+                        $searchSqlAll = $conn->query('SELECT * FROM products WHERE p_qty > 5');
+                        $resultAll = $searchSqlAll->fetchAll(PDO::FETCH_ASSOC);
                         $conn = null;
                         $num = 1;
-                    ?>
-                    <table class="table">
-                    <thead class="thead-dark">
-                        <tr>
-                            <th scope="col">ردیف</th>
-                            <th scope="col">کدینگ</th>
-                            <th scope="col">نام</th>
-                            <th scope="col">استقرار</th>
-                            <th scope="col">واحد</th>
-                            <th scope="col">تعداد</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                            <?php foreach ($resultAll as $row): ?>
+                        ?>
+                        <table class="table">
+                            <thead class="thead-dark">
                                 <tr>
-                                    <th scope="row"><?php echo $num++ ?></th>
-                                    <td><?php if(htmlspecialchars($row['p_codeing'])==0) {echo "ندارد";} else {echo htmlspecialchars($row['p_codeing']);}?></td>
-                                    <td><?php echo htmlspecialchars($row['p_name']) ?></td>
-                                    <td><?php echo htmlspecialchars($row['p_place']) ?></td>
-                                    <td><?php echo htmlspecialchars($row['p_unit']) ?></td>
-                                    <td><?php echo htmlspecialchars($row['p_qty']) ?></td>
+                                    <th scope="col">ردیف</th>
+                                    <th scope="col">کدینگ</th>
+                                    <th scope="col">نام</th>
+                                    <th scope="col">استقرار</th>
+                                    <th scope="col">واحد</th>
+                                    <th scope="col">تعداد</th>
+                                    <th scope="col" style="color: red;">حذف</th>
+                                    <th scope="col" style="color: blue;">اصلاح</th>
                                 </tr>
-                            <?php endforeach; ?>
-                    </tbody>
-                </table>
+                            </thead>
+                            <tbody>
+                                <?php foreach ($resultAll as $row): ?>
+                                    <tr>
+                                        <th scope="row"><?php echo $num++ ?></th>
+                                        <td><?php if (htmlspecialchars($row['p_codeing']) == 0) {
+                                                echo "ندارد";
+                                            } else {
+                                                echo htmlspecialchars($row['p_codeing']);
+                                            } ?></td>
+                                        <td><?php echo htmlspecialchars($row['p_name']) ?></td>
+                                        <td><?php echo htmlspecialchars($row['p_place']) ?></td>
+                                        <td><?php echo htmlspecialchars($row['p_unit']) ?></td>
+                                        <td><?php echo htmlspecialchars($row['p_qty']) ?></td>
+                                        <td><a style="color: black;" href="?id=<?php echo htmlspecialchars($row['id']) ?>"><i style="margin-right: 5px;" onmouseout="this.style.color='black';" onmouseover="this.style.color='red';" class="fa-thin fa-bin-recycle"></i></a></td>
+                                        <td><a style="color: black;" href="#"><i style="margin-right: 10px;" onmouseout="this.style.color='black';" onmouseover="this.style.color='blue';" class="fa-thin fa-pen-to-square edit-icon"></i></a></td>
+                                    </tr>
+                                <?php endforeach; ?>
+                            </tbody>
+                        </table>
 
-                </form>
-            </div>
-        </section>
-    </main>
-<script src="../Assets/Js/bootstrap.bundle.min.js"></script>
-<script src="../Assets/Js/bootstrap.min.js"></script>
-</body>
-</html>
+                    </form>
+                </div>
+            </section>
+        </main>
+        <script src="../Assets/Js/bootstrap.bundle.min.js"></script>
+        <script src="../Assets/Js/bootstrap.min.js"></script>
+    </body>
+
+    </html>
 <?php
-    }
+}
 ?>
